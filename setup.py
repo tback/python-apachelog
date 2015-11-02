4ab9250592ef33e723300cd3a45d16b548fb56b3
Till Backhaus <tback@tfw.ag>
2015-11-02 18:55:44 +0100

    Fork

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ from distutils.core import setup
 # From inspecting Mark Pilgrims feedparser...
 # patch distutils if it can't cope with the "classifiers" or "download_url"
 # keywords (prior to python 2.3.0).
+#
+# A note to Harry:
+# I'm not trying to steal your work. The project was archived on google.
+# I wanted python3 compatibility. I changed what was necessary and pushed it
+# to pypi under a new name. Feel free to contact me if you think I could have
+# done better.
+#
 from distutils.dist import DistributionMetadata
 if not hasattr(DistributionMetadata,'classifiers'):
     DistributionMetadata.classifiers = None
     
 setup(
-    name = 'apachelog',
-    version = '1.1',
+    name = 'tback-apachelog',
+    version = '1.2',
     description = 'Access log parser in python, ported from '\
     'Peter Hickman\'s Apache::LogRegex Perl moduleUniversal.',
     long_description = """\
@@ -28,7 +35,9 @@ in the log format.
 """,
     author='Harry Fuecks',
     author_email = 'hfuecks@gmail.com',
-    url = 'http://code.google.com/p/apachelog',
+    maintainer = 'Till Backhaus',
+    maintainer_email = 'till@backha.us',
+    url = 'https://github.com/tback/apachelog',
     license = "Artistic License / GPLv2",
     platforms = ['POSIX', 'Windows'],
     keywords = ['apache', 'log', 'parser'],
